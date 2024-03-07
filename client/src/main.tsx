0a5b4dba824c8d431a6624b5fbef981c4f28c7b2
kayYZ1 <overek7133@gmail.com>
2024-03-07 14:58:00 +0100

    chore: add inter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import '@fontsource/inter';
+
+import "global.css"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
