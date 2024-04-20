5d2dcc7ba90d4715ac9dc3ffb9e676b233957b8d
kayYZ1 <overek7133@gmail.com>
2024-04-20 23:01:07 +0200

    feat(topic): add topic btn

--- client/src/features/courses/single-course/components/topicsList.tsx
+++ client/src/features/courses/single-course/components/topicsList.tsx
@@ -1,9 +1,15 @@
+import { useSelector } from 'react-redux';
 import Stack from '@mui/joy/Stack';
+import { Typography, Box, IconButton } from '@mui/joy';
 
 import { Topic } from 'shared/ts/types';
 import TopicItem from './topicItem';
+import { selectCurrentUser } from 'app/users/user.slice';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function TopicsList({ topics }: { topics: Topic[] }) {
+  const user = useSelector(selectCurrentUser)
+
   return (
     <Stack
       spacing={2}
@@ -11,6 +17,14 @@ export default function TopicsList({ topics }: { topics: Topic[] }) {
         maxWidth: '80ch',
       }}
     >
+      {user.role === 'instructor' ?
+        <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
+          <Typography sx={{ px: 1, py: 1 }}>Add topic</Typography>
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </Box>
+        : ""}
       {topics.map((item: Topic) => (
         <TopicItem {...item} key={item.id} />
       ))}
