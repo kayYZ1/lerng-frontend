52de3edf82d307ec09dd6c21d1450016de091db8
kayYZ1 <overek7133@gmail.com>
2024-07-04 13:00:33 +0200

    feat: new landing-page

--- client/src/features/landing-page/index.tsx
+++ client/src/features/landing-page/index.tsx
@@ -1,40 +1,137 @@
-import { Box } from '@mui/joy';
-import Button from '@mui/joy/Button';
-import Typography from '@mui/joy/Typography';
-import ArrowForward from '@mui/icons-material/ArrowForward';
+import { Box, Stack, Typography, AspectRatio, Container, Button, Avatar, AvatarGroup } from '@mui/joy';
+import { ArrowForward } from '@mui/icons-material';
 import { useNavigate, Link } from 'react-router-dom';
 import { AuthPath } from 'routes/paths';
 import ColorSchemeToggle from 'shared/components/colorToggle';
+
+import lpImage from "assets/landingpage_img.png";
 
 export default function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <Box>
-      <Typography
-        level="h1"
-        fontWeight="xl"
+    <Box paddingTop={"1vh"}>
+      <Box component={"nav"} sx={{
+        height: "5vh", boxShadow: "-4px -4px 50px -20px rgba(66, 68, 90, 1);", padding: "1vh 1vw 1vh 1vw"
+      }}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography level="title-md" paddingTop={"0.5vh"}>Learn Linux</Typography>
+          <Box>
+            <ColorSchemeToggle />
+          </Box>
+        </Stack>
+      </Box>
+      <Container
+        sx={(theme) => ({
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          py: 10,
+          gap: 4,
+          [theme.breakpoints.up(834)]: {
+            flexDirection: 'row',
+            gap: 6,
+          },
+          [theme.breakpoints.up(1199)]: {
+            gap: 12,
+          },
+        })}
       >
-        Learn-Linux.com
-      </Typography>
-      <ColorSchemeToggle />
-      <Typography fontSize="lg" textColor="text.secondary" lineHeight="lg">
-        Welcome to Learn-Linux, your ultimate destination for mastering the ins and outs of the Linux operating system.
-      </Typography>
-      <Button
-        size="lg"
-        endDecorator={<ArrowForward />}
-        sx={{ mt: 2, mb: 1 }}
-        onClick={() => navigate(`${AuthPath.SIGN_UP}`)}
-      >
-        Join
-      </Button>
-      <Typography>
-        Already have an account?
-        <Link to={AuthPath.SIGN_IN} style={{ color: "#0b6bcb", textDecoration: "none", paddingLeft: "5px" }}>
-          Sign in
-        </Link>
-      </Typography>
+        <Box
+          sx={(theme) => ({
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '1rem',
+            maxWidth: '50ch',
+            textAlign: 'center',
+            flexShrink: 999,
+            [theme.breakpoints.up(834)]: {
+              minWidth: 420,
+              alignItems: 'flex-start',
+              textAlign: 'initial',
+            },
+          })}
+        >
+          <Typography color="primary" fontSize="lg" fontWeight="lg">
+            Embrace the power of learning
+          </Typography>
+          <Typography
+            level="h1"
+            fontWeight="xl"
+            fontSize="clamp(1.875rem, 1.3636rem + 2.1818vw, 3rem)"
+          >
+            Learn Linux is a complex e-learning web application
+          </Typography>
+          <Typography fontSize="lg" textColor="text.secondary" lineHeight="lg">
+            Allowing to learn specialized knowledge about linux and it's distrubution systems from professionals.
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              gap: 2,
+              my: 2,
+              '& > *': { flex: 'auto' },
+            }}
+          >
+            <Button size="lg" variant="outlined" color="neutral">
+              Learn More
+            </Button>
+            <Button size="lg" endDecorator={<ArrowForward />}>
+              <Link to={AuthPath.SIGN_UP} style={{ textDecoration: "none", color: "inherit" }}>
+                Get started
+              </Link>
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              gap: 2,
+              textAlign: 'left',
+              '& > *': {
+                flexShrink: 0,
+              },
+            }}
+          >
+            <AvatarGroup size="lg">
+              <Avatar />
+              <Avatar />
+              <Avatar />
+            </AvatarGroup>
+            <Typography textColor="text.secondary">
+              Join a community of over <b>10K</b> <br />
+              designers and developers.
+            </Typography>
+          </Box>
+        </Box>
+        <AspectRatio
+          ratio={600 / 520}
+          variant="outlined"
+          maxHeight={300}
+          sx={(theme) => ({
+            minWidth: 300,
+            alignSelf: 'stretch',
+            [theme.breakpoints.up(834)]: {
+              alignSelf: 'initial',
+              flexGrow: 1,
+              '--AspectRatio-maxHeight': '520px',
+              '--AspectRatio-minHeight': '400px',
+            },
+            borderRadius: 'sm',
+            bgcolor: 'background.level2',
+            flexBasis: '50%',
+          })}
+        >
+          <img
+            src={lpImage}
+            alt="Boy sitting in front of pc minding his bussiness."
+          />
+        </AspectRatio>
+      </Container>
     </Box>
   );
 }
