3fd0a334d32641b8cdb37fa973c5fdc9f75da88f
kayYZ1 <overek7133@gmail.com>
2024-09-01 18:05:07 +0200

    refactor(side-nav): refactor side nav list

--- src/layouts/dashboard/utils/roleBasedList.tsx
+++ src/layouts/dashboard/utils/roleBasedList.tsx
@@ -1,21 +1,8 @@
-import { Link } from 'react-router-dom';
-
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton from '@mui/joy/ListItemButton';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import ListItemContent from '@mui/joy/ListItemContent';
-
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import FeedbackIcon from '@mui/icons-material/Feedback';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import SignpostIcon from '@mui/icons-material/Signpost';
-
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from 'app/slice/user.slice';
 
-import { CoursesPath, DashboardPath } from 'routes/paths';
-import style from '../dashboard.module.css';
+import InstructorList from './instructorList';
+import UserList from './userList';
 
 export default function RoleBasedList() {
   const user = useSelector(selectCurrentUser);
@@ -24,78 +11,4 @@ export default function RoleBasedList() {
     case 'instructor': return <InstructorList />
     case 'user': return <UserList />
   }
-}
-
-function InstructorList() {
-  return (
-    <List
-      aria-labelledby="nav-list-browse"
-      sx={{
-        '& .JoyListItemButton-root': { p: '8px' },
-      }}
-    >
-      <ListItem>
-        <ListItemButton>
-          <ListItemDecorator>
-            <DashboardIcon fontSize="small" />
-          </ListItemDecorator>
-          <ListItemContent>
-            <Link to={DashboardPath.DASHBOARD} className={style.link}>Instructor panel</Link>
-          </ListItemContent>
-        </ListItemButton>
-      </ListItem>
-      <ListItem>
-        <ListItemButton>
-          <ListItemDecorator>
-            <SignpostIcon fontSize="small" />
-          </ListItemDecorator>
-          <ListItemContent>
-            <Link to={CoursesPath.COURSES} className={style.link}>Courses</Link>
-          </ListItemContent>
-        </ListItemButton>
-      </ListItem>
-    </List>
-  )
-}
-
-function UserList() {
-  return (
-    <List
-      aria-labelledby="nav-list-browse"
-      sx={{
-        '& .JoyListItemButton-root': { p: '8px' },
-      }}
-    >
-      <ListItem>
-        <ListItemButton>
-          <ListItemDecorator>
-            <LibraryBooksIcon fontSize="small" />
-          </ListItemDecorator>
-          <ListItemContent>
-            <Link to={DashboardPath.ENROLLED} className={style.link}>Enrolled</Link>
-          </ListItemContent>
-        </ListItemButton>
-      </ListItem>
-      <ListItem>
-        <ListItemButton>
-          <ListItemDecorator>
-            <SignpostIcon fontSize="small" />
-          </ListItemDecorator>
-          <ListItemContent>
-            <Link to={CoursesPath.COURSES} className={style.link}>Courses</Link>
-          </ListItemContent>
-        </ListItemButton>
-      </ListItem>
-      <ListItem>
-        <ListItemButton>
-          <ListItemDecorator>
-            <FeedbackIcon fontSize="small" />
-          </ListItemDecorator>
-          <ListItemContent>
-            <Link to={DashboardPath.FEEDBACK} className={style.link}>Feedback</Link>
-          </ListItemContent>
-        </ListItemButton>
-      </ListItem>
-    </List>
-  )
 }
