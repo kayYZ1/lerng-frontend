f96a1c723f1b6a1636efb316b2d47c767ab898e2
kayYZ1 <overek7133@gmail.com>
2024-09-02 22:20:40 +0200

    refactor(course): refactoring course card types

--- src/app/api/courses.api.slice.ts
+++ src/app/api/courses.api.slice.ts
@@ -1,5 +1,4 @@
 import { authApi } from 'app/base/auth.api';
-import { setCourses } from 'app/slice/courses.slice';
 
 export const coursesApiSlice = authApi.injectEndpoints({
   endpoints: (builder) => ({
--- src/layouts/dashboard/components/header.tsx
+++ src/layouts/dashboard/components/header.tsx
@@ -26,7 +26,13 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 import { useGetMeQuery, useSignOutFnMutation } from 'app/api/auth.api.slice';
 import { signOut } from 'app/slice/auth.slice';
+<<<<<<< HEAD
 import { authApi } from 'app/base/auth.api';
+=======
+import PageSelect from '../utils/pageSelect';
+
+import LerngLogo from "assets/svg/logo-no-background.svg";
+>>>>>>> a7e0719 (fix(build): remove setCourses)
 
 import { DashboardPath, AuthPath } from 'routes/paths';
 
@@ -42,7 +48,12 @@ export default function Header() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+<<<<<<< HEAD
   const { data, isLoading } = useGetMeQuery("User");
+=======
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetMeQuery(undefined);
+>>>>>>> a7e0719 (fix(build): remove setCourses)
   const [SignOutFn] = useSignOutFnMutation();
 
   const [open, setOpen] = useState(false);
