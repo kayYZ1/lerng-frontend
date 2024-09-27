2ff12bf1583a6ea16168ae9eb27732e3fd90733d
kayYZ1 <overek7133@gmail.com>
2024-09-27 14:29:33 +0200

    tests(reset-password): add unit tests

--- src/tests/forms/reset-password.test.tsx
+++ src/tests/forms/reset-password.test.tsx
@@ -1,22 +1,61 @@
 import { describe, expect, it } from 'vitest';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { render } from 'tests/wrapper';
 
 import ResetPassword from 'features/auth/forgot-password/reset-password';
+import { act } from 'react';
 
 describe('Password reset form tests', () => {
-  it('should render the component', () => {
+  //Load up component with all the providers
+  const renderDocument = () => {
     render(<ResetPassword />,
       {
         route: "/reset-password/token",
         path: "/reset-password/:token"
       }
     );
+  }
 
-    expect(screen.getByTestId("password")).toBeInTheDocument();
-    expect(screen.getByTestId("repeatPassword")).toBeInTheDocument();
-    expect(screen.getByRole('button', {
-      name: /reset/i
-    })).toBeInTheDocument();
+  it('should render the component', () => {
+    renderDocument();
+
+    const passwordInput = screen.getByPlaceholderText("New password")
+    const repeatPasswordInput = screen.getByPlaceholderText("Repeat your password")
+    const submitBtn = screen.getByRole('button', { name: /reset/i });
+
+    expect(passwordInput).toBeInTheDocument();
+    expect(repeatPasswordInput).toBeInTheDocument();
+    expect(submitBtn).toBeInTheDocument();
   });
-});
+
+  it('should validate users input', async () => {
+    renderDocument();
+
+    const submitBtn = screen.getByRole('button', { name: /reset/i });
+    fireEvent.click(submitBtn);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("password-error")).toBeInTheDocument();
+    });
+  });
+
+  it('should return error on password missmatch', async () => {
+    renderDocument();
+
+    const passwordInput = screen.getByPlaceholderText("New password")
+    const repeatPasswordInput = screen.getByPlaceholderText("Repeat your password")
+    const submitBtn = screen.getByRole('button', { name: /reset/i });
+
+    fireEvent.change(passwordInput), {
+      target: { value: "password123" }
+    }
+    fireEvent.change(repeatPasswordInput), {
+      target: { value: "password1234" }
+    }
+    fireEvent.click(submitBtn);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("rpassword-error")).toBeInTheDocument();
+    });
+  });
+})
